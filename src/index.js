3be4d6316311402c0cbf7c57a740c633d1bd9d17
paulhabiger <37312258+paulhabiger@users.noreply.github.com>
2018-03-21 22:15:00 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
+    registerServiceWorker();
+
